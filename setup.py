4f9f5b74d5eb83414ac2acd921341e14c9cc87b7
Виталий <vital@vital-nb.(none)>
2012-09-19 13:33:07 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='django-options',
+      version='0.0.1',
+      description='Options for django admin. Administrator emails, phones, contact data, etc.',
+      long_description='',
+      author='Vital Belikov',
+      author_email='vital@qwe.lv',
+      url='https://github.com/Brick85/options/',
+      include_package_data = True,
+      zip_safe = False,
+      requires=['django(>=1.3)'],
+      classifiers=['Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License'],
+      license='New BSD')

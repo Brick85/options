6e6bc6971acc9d9a1ea5ce3d88e771d155a6efee
Vital Belikov <vital@qwe.lv>
2013-01-14 20:36:26 +0200

    Template tags

--- options/templatetags/options_tags.py
+++ options/templatetags/options_tags.py
@@ -1,13 +1,27 @@
 from django import template
-from options.models import Option
+from options import get_option as get_option_source, get_label as get_label_source, get_text as get_text_source
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def getOption(context, key, as_var=None):
+def get_option(context, key, as_var=None):
+    return _get_qoption_value(get_option_source, context, key, as_var)
+
+
+@register.simple_tag(takes_context=True)
+def get_label(context, key, as_var=None):
+    return _get_qoption_value(get_label_source, context, key, as_var)
+
+
+@register.simple_tag(takes_context=True)
+def get_text(context, key, as_var=None):
+    return _get_qoption_value(get_text_source, context, key, as_var)
+
+
+def _get_qoption_value(function, context, key, as_var):
     try:
-        ret = Option.getValue(key)
+        ret = function(key)
     except:
         ret = ""
 

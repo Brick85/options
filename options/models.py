d19486b4779c8948881aa96d57adc45364a11a13
Vitaliy Belikov <vital@qwe.lv>
2023-07-09 11:15:00 +0300

    ugettext -> gettext

--- options/models.py
+++ options/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils.translation import get_language
 from django.core.cache import cache
 
